f14e102d53c99d709f3cb50677917418bf115c50
Matheus K <me@matheuskummel.com>
2018-05-12 12:43:22 -0300

    Command line - input from user

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,3 +3,24 @@ const _ = require('lodash');
 
 const notes = require('./notes');
 
+var command = process.argv[2];
+console.log('Command: ',command);
+console.log(process.argv);
+
+switch(command)
+{
+    case 'add':
+        console.log('Adding new note');
+        break;
+    case 'list':
+        console.log('Listing all notes');
+        break;
+    case 'read':
+        console.log('Reading note');
+        break;
+    case 'remove':
+        console.log('Removing note');
+        break;
+    default: 
+        console.log('Command not recognized');
+}
