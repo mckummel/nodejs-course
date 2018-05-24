aedf12a8680735650cd9cb43402789226aa21324
Matheus K <me@matheuskummel.com>
2018-05-24 14:46:49 -0300

    Section 4 - geocode API
    - geocode api
    - environment variable (so I won't send my key to github)
    - json stringify

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,12 @@
 const request = require('request');
 
-var cityUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=Porto%20Alegre%20Brasil';
+var geoApiKey = process.env.mapsGeoKey;
+
+var cityUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=Porto%20Alegre%20Brasil&key=${geoApiKey}`;
 
 request({
     url: cityUrl,
     json: true
 },(error, response, body)=>{
-    console.log(body);
+    console.log(JSON.stringify(body,undefined,2));
 });
