1186ce61b672ea5fc9d9af8bde35ff4100190c75
Matheus K <me@matheuskummel.com>
2018-05-27 12:27:25 -0300

    Section 4 - http request, getting lat and lng

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,5 +8,8 @@ request({
     url: cityUrl,
     json: true
 },(error, response, body)=>{
-    console.log(JSON.stringify(body,undefined,2));
+    //console.log(JSON.stringify(body,undefined,2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
 });
