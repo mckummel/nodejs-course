047daf45351440c2a58c6621e85f891121e8d68e
Matheus K <me@matheuskummel.com>
2018-05-28 12:02:22 -0300

    Section 4 - callback errors
    - geocode api request status errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -22,8 +22,36 @@ request({
     url: cityUrl,
     json: true
 },(error, response, body)=>{
-    //console.log(JSON.stringify(body,undefined,2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    if(error)
+    {
+        console.log('Unable to connect to google servers.');
+    }
+    else if(body.status === 'ZERO_RESULTS')
+    {
+        console.log('Unable to find that address');
+    }
+    else if(body.status === 'OVER_QUERY_LIMIT')
+    {
+        console.log('Quota limit reached');
+    }
+    else if(body.status === 'REQUEST_DENIED')
+    {
+        console.log('Request denied');
+    }
+    else if(body.status === 'INVALID_REQUEST')
+    {
+        console.log('Address, components or latlng are missing');
+    }
+    else if(body.status === 'UNKNOWN_ERROR')
+    {
+        console.log('A server error occurred, try again.');
+    }
+    else if(body.status === 'OK')
+    {
+        //console.log(JSON.stringify(body,undefined,2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    }
+    
 });
