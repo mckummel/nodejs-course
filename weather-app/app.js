6e479a4fc7028eda438cf6033093f0dcc3a53af5
Matheus K <me@matheuskummel.com>
2018-05-28 17:11:27 -0300

    Section 4 - refactor and callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,57 +1,22 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs.options({
-    a: {
-        demand: true,
-        alias: 'address',
-        describe: 'Address to fetch weather for',
-        string: true
-    }
-})
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
     .help()
-    .alias('help','h')
+    .alias('help', 'h')
     .argv;
 
-var address = encodeURIComponent(argv.a);
-var geoApiKey = process.env.mapsGeoKey;
-
-var cityUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${geoApiKey}`;
-
-request({
-    url: cityUrl,
-    json: true
-},(error, response, body)=>{
-    if(error)
-    {
-        console.log('Unable to connect to google servers.');
+geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
-    else if(body.status === 'ZERO_RESULTS')
-    {
-        console.log('Unable to find that address');
-    }
-    else if(body.status === 'OVER_QUERY_LIMIT')
-    {
-        console.log('Quota limit reached');
-    }
-    else if(body.status === 'REQUEST_DENIED')
-    {
-        console.log('Request denied');
-    }
-    else if(body.status === 'INVALID_REQUEST')
-    {
-        console.log('Address, components or latlng are missing');
-    }
-    else if(body.status === 'UNKNOWN_ERROR')
-    {
-        console.log('A server error occurred, try again.');
-    }
-    else if(body.status === 'OK')
-    {
-        //console.log(JSON.stringify(body,undefined,2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
-    }
-    
 });
---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,37 @@
+const request = require('request');
+
+var geocodeAddress = (inputAddress, callback) => {
+    var address = encodeURIComponent(inputAddress);
+    var geoApiKey = process.env.mapsGeoKey;
+
+    var cityUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${geoApiKey}`;
+
+    request({
+        url: cityUrl,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            callback('Unable to connect to google servers.');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('Unable to find that address');
+        } else if (body.status === 'OVER_QUERY_LIMIT') {
+            callback('Quota limit reached');
+        } else if (body.status === 'REQUEST_DENIED') {
+            callback('Request denied');
+        } else if (body.status === 'INVALID_REQUEST') {
+            callback('Address, components or latlng are missing');
+        } else if (body.status === 'UNKNOWN_ERROR') {
+            callbackg('A server error occurred, try again.');
+        } else if (body.status === 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            })
+        }
+    });
+};
+
+module.exports = {
+    geocodeAddress: geocodeAddress
+};
